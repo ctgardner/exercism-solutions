1076ee591daaed6ff6f17b3f9b8c9e05fbaec249
ctgardner <10970827+ctgardner@users.noreply.github.com>
2026-05-12 16:31:40 +1000

    Solve go/welcome-to-tech-palace

---
+++ go/welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -0,0 +1,21 @@
+package techpalace
+
+import "strings"
+
+const preamble = "Welcome to the Tech Palace, "
+
+// WelcomeMessage returns a welcome message for the customer.
+func WelcomeMessage(customer string) string {
+	return preamble + strings.ToUpper(customer)
+}
+
+// AddBorder adds a border to a welcome message.
+func AddBorder(welcomeMsg string, numStarsPerLine int) string {
+	border := strings.Repeat("*", numStarsPerLine)
+	return strings.Join([]string{border, welcomeMsg, border}, "\n")
+}
+
+// CleanupMessage cleans up an old marketing message.
+func CleanupMessage(oldMsg string) string {
+	return strings.TrimSpace(strings.ReplaceAll(oldMsg, "*", ""))
+}
