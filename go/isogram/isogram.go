16f420bc8860992575c2bbdf8a47dc91f16dd8b4
ctgardner <10970827+ctgardner@users.noreply.github.com>
2026-05-15 12:02:53 +1000

    Solve go/isogram

---
+++ go/isogram/isogram.go
@@ -0,0 +1,21 @@
+package isogram
+
+func IsIsogram(word string) bool {
+	var bitfield uint32 = 0
+	for _, r := range word {
+		if r == ' ' || r == '-' {
+			continue
+		}
+
+		if r >= 'a' && r <= 'z' {
+			r -= 32
+		}
+
+		mask := uint32(1 << (r - 'A'))
+		if bitfield&mask != 0 {
+			return false
+		}
+		bitfield |= mask
+	}
+	return true
+}
