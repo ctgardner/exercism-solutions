0b2cf363bd8490163e539e13325ad06d35eeafee
ctgardner <10970827+ctgardner@users.noreply.github.com>
2026-06-30 16:00:20 +1000

    Solve go/reverse-string

---
+++ go/reverse-string/reverse_string.go
@@ -0,0 +1,9 @@
+package reversestring
+
+func Reverse(input string) string {
+	rs := []rune(input)
+	for i, j := 0, len(rs)-1; i < j; i, j = i+1, j-1 {
+		rs[i], rs[j] = rs[j], rs[i]
+	}
+	return string(rs)
+}
